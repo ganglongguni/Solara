ab890d30f7aa946df8fa3843530170c2bf544233
ganglongguni <1556866926@qq.com>
2026-09-01 12:47:45 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -648,7 +648,7 @@ function buildAudioProxyUrl(url) {
 }
 
 const SOURCE_OPTIONS = [
-    { value: "netease", label: "网易云音乐1" },
+    { value: "netease", label: "网易云音乐" },
     { value: "kuwo", label: "酷我音乐" },
     { value: "joox", label: "JOOX音乐" },
     { value: "bilibili", label: "哔哩哔哩" },
